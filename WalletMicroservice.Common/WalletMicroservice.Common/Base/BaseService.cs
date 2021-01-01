5be105d021c631e86a2833ad5b7c96f64c80cb5c
bioyeneye <bioyeneye@gmail.com>
2021-01-01 04:23:48 +0100

    finished base service

--- WalletMicroservice.Common/WalletMicroservice.Common/Base/BaseService.cs
+++ WalletMicroservice.Common/WalletMicroservice.Common/Base/BaseService.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace WalletMicroservice.Common.Base
 {
-    class BaseService
+    public abstract class BaseService<T>
     {
+        private readonly ILogger<T> _logger;
+
+        protected BaseService(ILogger<T> logger)
+        {
+            _logger = logger;
+        }
+
+        protected void LogInformation(string message, object data)
+        {
+            _logger.LogInformation($"{message}: \n{JsonConvert.SerializeObject(data, Formatting.Indented)}\n");
+        }
+
+        protected void LogError(Exception ex, string message)
+        {
+            _logger.LogError(ex, $"\n {message}");
+        }
     }
+
 }
